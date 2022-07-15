73c6da897ebb556379105ef52ad378d16b018e37
onmidnightblue <wavesofdawn@nate.com>
2022-07-15 11:41:33 +0900

    사용하지 않는 hook 제거

--- src/components/Cookie/Cookies.jsx
+++ src/components/Cookie/Cookies.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Cookie, Open } from "../../assets";
 import styled from "styled-components";
 import axios from "axios";
